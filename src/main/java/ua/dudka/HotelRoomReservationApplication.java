db77f309d5601463de063adcc983ef7b60da2c09
rostislav.dudka <rostislav.dudka@gmail.com>
2017-06-10 15:00:23 +0300

    -initial commit;

---
+++ src/main/java/ua/dudka/HotelRoomReservationApplication.java
@@ -0,0 +1,12 @@
+package ua.dudka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomReservationApplication.class, args);
+	}
+}

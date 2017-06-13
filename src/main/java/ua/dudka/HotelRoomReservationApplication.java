c51d9471d5dd2e36d5147c04ca2c5f7ac32aa554
rostislav.dudka <rostislav.dudka@gmail.com>
2017-06-13 11:48:33 +0300

    -added security;

--- src/main/java/ua/dudka/HotelRoomReservationApplication.java
+++ src/main/java/ua/dudka/HotelRoomReservationApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import ua.dudka.domain.room.HotelRoom;
 import ua.dudka.repository.room.HotelRoomRepository;
+
+import static ua.dudka.web.booking.CreateBookingRequestController.Links.CREATE_BOOKING_REQUEST_PAGE_URL;
+import static ua.dudka.web.room.HotelRoomController.Links.ROOM_PAGE_URL;
 
 @SpringBootApplication
 public class HotelRoomReservationApplication {
@@ -34,6 +44,53 @@ public class HotelRoomReservationApplication {
 				}
 				repository.save(s);
 			}
+		}
+	}
+
+
+	@Configuration
+	@Profile("security")
+	@EnableWebSecurity
+	@Controller
+	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@GetMapping("/login")
+		public String getLoginPage() {
+			return "login";
+		}
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication()
+					.withUser("admin")
+					.password("admin")
+					.roles("ADMIN");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.csrf()
+					.and()
+					.authorizeRequests()
+					.antMatchers("/**").permitAll()
+					.antMatchers("/admin").hasRole("ADMIN")
+					.anyRequest().authenticated()
+					.and()
+					.formLogin()
+					.loginPage("/login")
+					.successForwardUrl(ROOM_PAGE_URL)
+					.permitAll()
+					.and()
+					.logout()
+					.permitAll();
+		}
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web
+					.ignoring()
+					.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/fonts/**");
 		}
 	}
 }
